dbe66d2312aa2b835b7397197db54df1c42099d1
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-04 18:15:09 -0500

    added bootstrap and fontawesome to head, began pseudo

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// HTML
+// create a header that says Weather Dashboard
+// separate into two columns for search and results (3, 9?)
+// - search section: need to search by city name, needs to retain recent searches
+// - must then search API for proper weather results and display in next section
+// - results section:
+// - container for various weather results of city (temp, humidity, etc.)
+// - 5-day forecast (in card format? less info per card)
+
+// JS
+// link jQuery to HTML
+// create const/var for various elements
+// - - open weather api key
+// - - temp, humidity, wind speed, uv-index
+// - - history, five-day forecast, search button
+// create funciton to get weather of cityName from Open Weather API
+// - displays current weather
+// - within the same function, get uv index and 5 day forecast
+
+// CSS
