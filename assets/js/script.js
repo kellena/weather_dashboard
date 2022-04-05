0d032364dbf9694a08b8e32a504e799725a61d1a
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-04 21:41:36 -0500

    got way too lost and was too behind conceptually to get code working, will revisit assignment at a later date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
+var searchCity = document.getElementById("searchCity")
+var searchButton = document.getElementById("searchBtn")
+var searchHistory = document.getElementById("searchHistory")
+var apiKey = "fcfd8f094c533b7224130fcd283eee7b";
+
+// write a function to get searchHistory and display under search bar
+
+
+
+// write function to get current weather, and then display
+
 // HTML
 // create a header that says Weather Dashboard
 // separate into two columns for search and results (3, 9?)
 // - search section: need to search by city name, needs to retain recent searches
+// - on button press, set default, display in same section as search
+// - get city name, take lat and lon from api, input it back into url
 // - must then search API for proper weather results and display in next section
 // - results section:
 // - container for various weather results of city (temp, humidity, etc.)
