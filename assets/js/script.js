522d7c285ab85cb0526437510a89bff3f07a2403
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-25 21:25:03 -0500

    began writing function to display previous city searches, loop to build the list

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,24 @@ var searchBtn = document.getElementById("searchBtn")
 var searchHist = document.getElementById("searchHistory")
 var currentWeather = document.getElementById("currentWeather")
 var forecastWeather = document.getElementById("forecast")
+var cityList = document.getElementById("cityList")
+
 var apiKey = "fcfd8f094c533b7224130fcd283eee7b";
 
+var previousCities = [];
+
+function displayPreviousCities() {
+
+    cityList.innerHTML = "";
+
+       for (var i = 0; i < cityHistory.length; i++) {
+           var searchedCities = previousCities[i];
+   
+           var li = document.createElement('li');
+           li.classList.add('custom-li')
+           li.textContent = searchedCities;
+           li.setAttribute('data-index', i);
+           cityList.appendChild(li);   
+       }
+       
+}
