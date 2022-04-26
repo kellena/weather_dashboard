28c0382bdf953f9a54e139e5ccdb32e12afab346
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-25 21:18:31 -0500

    adjusted html and added some formatting to the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,7 @@
 var searchCity = document.getElementById("searchCity")
-var searchButton = document.getElementById("searchBtn")
-var searchHistory = document.getElementById("searchHistory")
+var searchBtn = document.getElementById("searchBtn")
+var searchHist = document.getElementById("searchHistory")
+var currentWeather = document.getElementById("currentWeather")
+var forecastWeather = document.getElementById("forecast")
 var apiKey = "fcfd8f094c533b7224130fcd283eee7b";
 
-// write a function to get searchHistory and display under search bar
-
-
-
-// write function to get current weather, and then display
-
-// HTML
-// create a header that says Weather Dashboard
-// separate into two columns for search and results (3, 9?)
-// - search section: need to search by city name, needs to retain recent searches
-// - on button press, set default, display in same section as search
-// - get city name, take lat and lon from api, input it back into url
-// - must then search API for proper weather results and display in next section
-// - results section:
-// - container for various weather results of city (temp, humidity, etc.)
-// - 5-day forecast (in card format? less info per card)
-
-// JS
-// link jQuery to HTML
-// create const/var for various elements
-// - - open weather api key
-// - - temp, humidity, wind speed, uv-index
-// - - history, five-day forecast, search button
-// create funciton to get weather of cityName from Open Weather API
-// - displays current weather
-// - within the same function, get uv index and 5 day forecast
-
-// CSS
